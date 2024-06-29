96a78d49c4f5bcad289aa166982f65927b69d24f
Asadullah-khan596 <asadullahkhan3096@gmail.com>
2024-06-29 17:32:08 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+ "use strict"; // It means treat all JS code as newer version
+
+//  alert(7+4) // we are using nodejs, not browser
+
+console.log(6+3) // code readability should be high
+
+let name = "asad"
+let age = 20
+let isLoggedIn = true
+
+/*
+number => 2 to power 53
+bigint
+string =>""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+*/
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
