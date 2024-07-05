035e1c939b00ec6fd31e243e0803b25db89c7de1
Asadullah-khan596 <asadullahkhan3096@gmail.com>
2024-07-05 18:32:17 +0000

    object_1

---
+++ 12_object_I.js
@@ -0,0 +1,39 @@
+//singleton -->only forms when we develop object from constructor not from literals--->A singleton is a function or class which can have only one instance.
+
+  //By using constructor
+  Object.create //---> discussed in next lecture
+ // object literals ---> way of declaring object
+
+
+ //defining symbol inside the object
+ const mySym=Symbol("fox1")
+ const jsUser={
+    name:"Asad",
+    age:21,
+    [mySym]:"star1",//correct way to declare a symbol inside the object without changing its datatype
+    location:"Hyderabad",
+    isLoggedIn:false,
+    lastLoginDAys: ["mon","sat","sun"]
+ }
+
+ //accessing data of objects
+
+ console.log(jsUser.name); //while using (.) dot operator for accessing no need to declare name as a string is will automatic understand
+
+ //mth-2 for accessing data inside the object
+ console.log(jsUser["name"]); //square notations
+
+console.log(jsUser[mySym]);//method of accessing the symbol from the object
+
+//freezing the editing and manuplation of an object
+// Object.freeze(jsUser);
+
+
+jsUser.greeting=function(){
+    console.log("hello don");
+}
+jsUser.greetingTwo=function(){
+    console.log(`hello ${this.name} Don`); //using object data inside the function
+}
+console.log(jsUser.greetingTwo());
+console.log(jsUser.greeting());
